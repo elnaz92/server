67197583f68797ebd7a4b0fae78eb67429b28088
elnaz92 <46791554+elnaz92@users.noreply.github.com>
2019-01-17 20:58:50 +0330

    Add files via upload

---
+++ db.js
@@ -0,0 +1,69 @@
+{
+	"health_data":[
+	{
+	"id":1,
+	"data":[
+		{"num": 10.989},
+		{"num": 99.898},
+		{"num": 85.989},
+		{"num": 54.533},
+		{"num": 34.223}
+		]
+	},
+	{
+	"id":2,
+	"data":[
+		{"num": 1.989},
+		{"num": 9.898},
+		{"num": 8.989},
+		{"num": 5.533},
+		{"num": 4.223}
+		]
+	},
+	{
+	"id":3,
+	"data":[
+		{"num": 0.989},
+		{"num": 0.898},
+		{"num": 0.989},
+		{"num": 0.533},
+		{"num": 0.223}
+		]
+	}
+	],
+	"user_data":[
+	{
+	"id":1,
+	"user":{
+		"username":"elnaz_h",
+		"password":"123",
+		"name":"elnaz",
+		"family":"hashemzadeh"
+	},
+	"age":26,
+	"doctor":"true"	
+	},
+	{
+	"id":2,
+	"user":{
+		"username":"shiva_k",
+		"password":"321",
+		"name":"shiva",
+		"family":"karimi"
+	},
+	"age":20,
+	"doctor":"false"	
+	},
+	{
+	"id":3,
+	"user":{
+		"username":"mojde_ra",
+		"password":"111",
+		"name":"mojde",
+		"family":"rabie"
+	},
+	"age":23,
+	"doctor":"true"	
+	}
+	]  
+}
